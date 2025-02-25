040622cff468978fec63e7cffe8b06d61fe9ea7c
Saad Raza <razasaad62186gmail.com>
2025-02-26 00:10:26 +0500

    first comment

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+    {
+        id : 1,
+        img : "/slide2.webp",
+        title : "SUMMER SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+    },
+    {
+        id : 2,
+        img : "/slide2.webp",
+        title : "WINTER SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+    },
+    {
+        id : 3,
+        img : "/slide2.webp",
+        title : "POPULAR SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+    },
+]
