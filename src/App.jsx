da85fc3d885f9d6ba1a08763f58f3bc6bffb74a9
Saad Raza <razasaad62186gmail.com>
2025-03-14 23:35:43 +0500

    added Styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import ProductList from './pages/ProductList'
 import Product from './pages/Product'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Cart from './pages/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Register />
+      <Home />
     </>
   )
 }
